1517030953145432367be85bb8b1d721a0b10722
NULL-header <poaoq.poaoq@gmail.com>
2022-04-18 16:43:33 +0900

    update for un working

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -6,7 +6,7 @@ export const HelloWorld = () => (
     <hr></hr>
     <h3>Environmental variables:</h3>
     <p>
-      process.env.PRODUCTION:<b>{process.env.PRODCTION?.toString()}</b>
+      process.env.PRODUCTION:<b>{process.env.PRODUCTION?.toString()}</b>
     </p>
     <p>
       process.env.NAME:<b>{process.env.NAME}</b>
--- webpack.config.ts
+++ webpack.config.ts
@@ -6,56 +6,54 @@ import packageJSON from "./package.json";
 const webpackConfig = (env: {
   production: any;
   development: any;
-}): webpack.Configuration => ({
-  entry: "./src/index.tsx",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  output: {
-    path: path.join(__dirname, "/public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        options: {
-          transpileOnly: true,
-          configFile: "tsconfig.build.json",
-        },
-        exclude: /public/,
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: { url: false },
+}): webpack.Configuration => {
+  return {
+    entry: "./src/index.tsx",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    output: {
+      path: path.join(__dirname, "/public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            configFile: "tsconfig.build.json",
           },
-        ],
-      },
-      {
-        test: /\.js/,
-        enforce: "pre",
-        loader: "source-map-loader",
-      },
+          exclude: /public/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: { url: false },
+            },
+          ],
+        },
+        {
+          test: /\.js/,
+          enforce: "pre",
+          loader: "source-map-loader",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./src/index.html" }),
+      new webpack.DefinePlugin({
+        "process.env.PRODUCTION": env.production || !env.development,
+        "process.env.NAME": JSON.stringify(packageJSON.name),
+        "process.env.VERSION": JSON.stringify(packageJSON.version),
+      }),
+      new ForkTsCheckerWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-    new webpack.DefinePlugin({
-      "process.env.PRODUCTION": env.production || !env.development,
-      "process.env.NAME": JSON.stringify(packageJSON.name),
-      "process.env.VERSION": JSON.stringify(packageJSON.version),
-    }),
-    new ForkTsCheckerWebpackPlugin(),
-  ],
-});
+  };
+};
 
 export default webpackConfig;
